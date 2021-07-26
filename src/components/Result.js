e25474f96e4755dc8f90a85f4af9669a1fe7da9c
Ralf Braitling <work@braitling.de>
2021-07-26 06:33:45 +0200

    feat: add styles

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,14 +1,30 @@
-
-function Result ({question}){
+import ProgressBar from "./Progressbar";
+function Result ({question, user}){
         const one = question.optionOne
         const two = question.optionTwo
-        const allvotes = one.votes.length + two.votes.length
+        const allVotes = one.votes.length + two.votes.length
+        const usersVote = one.votes.some((vote) => vote===user.userName) ? true : false;
+         
+
         return (
           <div>
-              <div>{one.text}</div>
-              <div>{one.votes.length} of {allvotes}</div>
-                <div>{two.text}</div>
-              <div>{two.votes.length} of {allvotes}</div>
+            <span className="head">Result</span>
+            <ProgressBar
+              result={one.votes.length}
+              total={allVotes}
+              width="362"
+              hasMarker={usersVote}
+            >
+              {one.text}
+            </ProgressBar>
+            <ProgressBar
+              result={two.votes.length}
+              total={allVotes}
+              width="362"
+              hasMarker={!usersVote}
+            >
+              {two.text}
+            </ProgressBar>
           </div>
         );
     
