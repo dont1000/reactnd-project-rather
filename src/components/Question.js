fd9790c27c6e1c06c0ea421d929e60f471a65f11
Ralf Braitling <work@braitling.de>
2021-07-26 06:34:28 +0200

    feat: add styles

--- src/components/Question.js
+++ src/components/Question.js
@@ -14,7 +14,7 @@ class Question extends Component{
 
     onSubmit = (event) =>{
       event.preventDefault();
-    const { question, dispatch } = this.props;
+      const { question, dispatch } = this.props;
       dispatch(handleQuestionAnswer({qid:question.id, answer:this.state.option }))
     }
 
@@ -22,28 +22,32 @@ class Question extends Component{
         const {question} = this.props
         return (
           <div>
+            <span className="head">Would you rather</span>
             <form onSubmit={this.onSubmit}>
-              <input
-                type="radio"
-                id="option1"
-                name="optionOne"
-                value="optionOne"
-                checked={this.state.option === "optionOne"}
-                onChange={this.onChange}
-              />
-              {question.optionOne.text}
-
-              <input
-                type="radio"
-                id="optionTwo"
-                name="optionTwo"
-                value="optionTwo"
-                checked={this.state.option === "optionTwo"}
-                onChange={this.onChange}
-              />
-              {question.optionTwo.text}
-
-              <button type="submit">Submit</button>
+             
+                <input
+                  type="radio"
+                  id="optionOne"
+                  name="optionOne"
+                  value="optionOne"
+                  checked={this.state.option === "optionOne"}
+                  onChange={this.onChange}
+                />
+                <label for="optionOne">{question.optionOne.text}</label>
+            
+                <input
+                  type="radio"
+                  id="optionTwo"
+                  name="optionTwo"
+                  value="optionTwo"
+                  checked={this.state.option === "optionTwo"}
+                  onChange={this.onChange}
+                />
+                <label for="optionTwo">{question.optionTwo.text}</label>
+           
+              <button className="button__cta" type="submit">
+                Submit
+              </button>
             </form>
           </div>
         );
