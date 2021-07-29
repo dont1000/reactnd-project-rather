47bf2a21f9aae87f92a643a90e271229aef2f164
Ralf Braitling <work@braitling.de>
2021-07-29 17:02:43 +0200

    feat: add standalone option for create question

--- src/components/AskAQuestion.js
+++ src/components/AskAQuestion.js
@@ -1,34 +1,49 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleSaveYourQuestion } from "../actions/questions";
+import { withRouter } from "react-router";
 import "./AskAQuestion.scss";
 
 class AskAQuestion extends Component {
   state = { 
         optionTwoText:"",
         optionOneText: "", 
-        showDialog: false
-     };
-  onSubmit = (event) => {
-    event.preventDefault();
-    const { dispatch } = this.props;
-    if (this.state.optionOneText === "" || this.state.optionTwoText==="") return;
-    dispatch(
-        handleSaveYourQuestion(
-          {
-            optionOneText: this.state.optionOneText,
-            optionTwoText: this.state.optionTwoText,
-          },
-          () => {
-            this.toggleDialog();
-            this.setState({
-              optionTwoText: "",
-              optionOneText: "",
-            });
-          }
-        )
-      );
-  };
+        showDialog: false,
+        showButton:true
+    };
+
+    componentDidMount(){
+        const { location } = this.props;
+ 
+        if (location.pathname ==="/add"){
+              this.setState({
+                ...this.state,
+                showDialog: true,
+                showButton:false
+              });
+        }
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        const { dispatch } = this.props;
+        if (this.state.optionOneText === "" || this.state.optionTwoText==="") return;
+        dispatch(
+            handleSaveYourQuestion(
+            {
+                optionOneText: this.state.optionOneText,
+                optionTwoText: this.state.optionTwoText,
+            },
+            () => {
+                this.toggleDialog();
+                this.setState({
+                    optionTwoText: "",
+                    optionOneText: "",
+                });
+                this.props.history.push('/'); 
+            })
+        );
+    };
   onChange = (event) => {
     this.setState({
       ...this.state,
@@ -37,24 +52,27 @@ class AskAQuestion extends Component {
   };
 
   toggleDialog = () => {
-    console.log("toggle");
     this.setState({
       ...this.state,
       showDialog: !this.state.showDialog,
     });
   };
 
+buttonClass = () => {
+    if(!this.state.showButton) return 'ask__add ask__add--hide';
+    return this.state.showDialog ? "ask__add ask__add--close" : "ask__add";
+  }
+
   render() {
     const { autheUser } = this.props;
+
     return (
       <Fragment>
         {autheUser && (
           <div className="ask">
             <div className="ask__header">
               <div
-                className={`ask__add ${
-                  this.state.showDialog ? "ask__add--close" : ""
-                }`}
+                className={this.buttonClass()}
                 onClick={this.toggleDialog}
               >+
               </div>
@@ -66,7 +84,7 @@ class AskAQuestion extends Component {
               onSubmit={this.onSubmit}
             >
             <legend>Create a new Question</legend>
-            <span class="ask_subline">Would you rather...</span>
+            <span className="ask_subline">Would you rather...</span>
               <input
                 type="text"
                 id="optionOneText"
@@ -100,4 +118,4 @@ function mapToProp({authedUser, users}) {
   const autheUser = users[authedUser.userName];
   return { autheUser};
 }
-export default connect(mapToProp)(AskAQuestion)
+export default withRouter(connect(mapToProp)(AskAQuestion));
