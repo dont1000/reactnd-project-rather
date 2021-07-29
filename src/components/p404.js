fa33e519a7507a09cb97ee8560d3c0c22a170993
Ralf Braitling <work@braitling.de>
2021-07-29 17:04:30 +0200

    feat: add 404 page

---
+++ src/components/p404.js
@@ -0,0 +1,12 @@
+
+
+import "./p404.scss"
+const p404=()=>{
+    return(
+        <div className="p404">
+           <div className="head">This question doesn't exist. </div>   
+        </div>
+    )
+}
+
+export default p404
