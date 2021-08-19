b59ec5bbe9f4dc634d70d4a81a9e307c0d16b501
Ralf Braitling <work@braitling.de>
2021-08-19 14:49:15 +0200

    feat: style  login view

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addAuthedUser } from "../actions/authedUser";
-import "./Login.css";
+import "./Login.scss";
 
 
 class Login extends Component {
@@ -28,30 +28,47 @@ class Login extends Component {
   handleSubmit= (event) => {
     const { dispatch } = this.props;
     event.preventDefault();
-    console.log("handle siubmit")
     dispatch(addAuthedUser(this.state.option));
   }
 
   render() {
     return (
-      <div className="logIn">
-        <select value={this.state.value} onChange={this.handleChange}>
-          <option value="">--Please choose an option--</option>
-          {this.users.map((user) => (
-            <option value={user.id}>{user.name}</option>
-          ))}
-        </select>
-        <button onClick={this.handleSubmit}>Submit</button>
+      <div className="logIn__wrapper">
+        <div className="logo">
+          <div className="logo__circle__wrapper">
+            <div className="logo__circle logo__circle--red" />
+            <div className="logo__circle logo__circle--green" />
+          </div>
+          <div>Would you rather</div>
+        </div>
+        <div className="logIn ">
+          <div className="select__wrapper">
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value=""> Please choose a user </option>
+              {this.users.map((user) => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div class="button__cta__wrapper--right">
+            <button
+              className="button__cta button__cta--static"
+              onClick={this.handleSubmit}
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-const maptoProp = ({authedUser}) => {
-return authedUser;
-}
 
-export default connect(maptoProp)(Login);
+
+export default connect()(Login);
 // select
 // cta
 // private route comp
